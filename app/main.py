1bac71f1fdd02239e2b05f6fb26993100c4b99ec
albertohco <acordeiro@3anet.com.br>
2023-10-15 20:15:48 -0300

    inclusão do load e o main funcionando

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 from pipeline.extract import ler_arquivos_excel
+from pipeline.transform import concat_dataframe
+from pipeline.load import load_excel
 
-path="app/data/input"
-data_frame_list = ler_arquivos_excel(path)
-print(data_frame_list)
+if __name__ == "__main__":
+    data_frame_list = ler_arquivos_excel("app/data/input")
+    print(type(data_frame_list))
+    data_frame = concat_dataframe(data_frame_list)
+    print(type(data_frame))
+    load_excel(data_frame, "app/data/output", "output")
